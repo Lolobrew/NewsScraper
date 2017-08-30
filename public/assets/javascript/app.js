36984894bdd53fd491d74c7b521ce20d188f5730
Jeff <jeffschultz.js@gmail.com>
2017-08-29 21:54:02 -0700

    finished routing, need to put thought into modal popup

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,9 +1,100 @@
 $('#scrapeBtn').on("click", function(){
-    $.getJSON("/articles", function(data) {
-        // For each one
-        for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
-          $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-        }
-      });
-})
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  }).done(function(data) {
+    //display articles after a timeout so it has time to retrieve info and post to articles api route
+    function showArticles() {
+      setTimeout(function(){
+        $.getJSON("/articles", function(data) {
+          // For each one
+          for (var i = 0; i < data.length; i++) {
+            // Display the apropos information on the page
+            $("#articles").append("<h2 data-id='" + data[i]._id + "'>" + data[i].title + "</h2><br/><h4>" + data[i].summary + "</h4><br/><img src='http://www.technewsworld.com/" + data[i].link + "'/><br>");
+          }
+              //empty btn div, dynamically create a clear button for deleting data
+              $('#btnDiv').empty();
+              $('#btnDiv').append("<btn class='btn btn-warning' id='clearIt'>Clear It</btn>");
+              $('#clearIt').on("click", function(){
+                $('#thoughts').empty();
+                $('#articles').empty();
+                $.ajax({
+                  method: "GET",
+                  url: "/delete"
+                }).done(function(data){
+                  window.location.replace("http://localhost:8080");
+                })
+              });
+        });
+      }, 2000);
+    }
+
+    showArticles();
+
+  });
+});
+
+
+
+// Whenever someone clicks a p tag
+$(document).on("click", "h2", function() {
+  // Empty the thoughts from the thought section
+  $("#thoughts").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the thought information to the page
+    .done(function(data) {
+      console.log(data);
+      // The title of the article
+      $("#thoughts").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#thoughts").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new thought body
+      $("#thoughts").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new thought, with the id of the article saved to it
+      $("#thoughts").append("<button data-id='" + data._id + "' id='savethought'>Save Thought</button>");
+
+      // If there's a thought in the article
+      if (data.thought) {
+        // Place the title of the thought in the title input
+        $("#titleinput").val(data.thought.title);
+        // Place the body of the thought in the body textarea
+        $("#bodyinput").val(data.thought.body);
+      }
+    });
+});
+
+// When you click the savethought button
+$(document).on("click", "#savethought", function() {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the thought, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from thought textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .done(function(data) {
+      // Log the response
+      console.log(data);
+      // Empty the thoughts section
+      $("#thoughts").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for thought entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
--- routes/all-routes.js
+++ routes/all-routes.js
@@ -2,6 +2,7 @@ var path = require ("path");
 var request = require("request");
 var cheerio = require("cheerio");
 var Article = require("../models/Article.js");
+var Thought = require("../models/Thought.js");
 
 module.exports = function(app) {
 
@@ -16,15 +17,32 @@ module.exports = function(app) {
 
 
     app.get("/scrape", function(req, res){
-        request("http://www.theonion.com/section/local/", function(error, response, html) {
+        request("http://www.technewsworld.com/", function(error, response, html) {
             var $ = cheerio.load(html);
 
-            $("div .info").each(function(i, element) {
+            $("div .story-list").each(function(i, element) {
                 var result = {};
-                result.title = $(this).children("h2 .headline").text();
-                result.summary = $(this).children("div .desc").text();
-                result.link = $(this).children("img").attr("src");
 
+                //title w/ error handling
+                if ($(this).children(".title").children("a").text() == ""){
+                  result.title = "undefined";
+                } else {
+                  result.title = $(this).children(".title").children("a").text();
+                }
+                //summary w/ error handling
+                if ($(this).children(".teaser").text() == ""){
+                  result.summary = "undefined";
+                } else {
+                  result.summary = $(this).children(".teaser").text();
+                }
+                //image (link) w/ error handling
+                if ($(this).children(".image").children("a").children("img").attr("src") == ""){
+                  result.link = "undefined"
+                } else {
+                  result.link = $(this).children(".image").children("a").children("img").attr("src");
+                }
+                
+              console.log(result);
                 var entry = new Article(result);
                 entry.save(function(err, doc) {
                     if (err) {
@@ -40,73 +58,87 @@ module.exports = function(app) {
     });
 
     
+    // This will get the articles we scraped from the mongoDB
+    app.get("/articles", function(req, res) {
+      // Grab every doc in the Articles array
+      Article.find({}, function(error, doc) {
+        // Log any errors
+        if (error) {
+          console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+          res.json(doc);
+        }
+      });
+    });
+
+
+    // Grab an article by it's ObjectId
+    app.get("/articles/:id", function(req, res) {
+      // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+      Article.findOne({ "_id": req.params.id })
+      // ..and populate all of the thoughts associated with it
+      .populate("thought")
+      // now, execute our query
+      .exec(function(error, doc) {
+        // Log any errors
+        if (error) {
+          console.log(error);
+        }
+        // Otherwise, send the doc to the browser as a json object
+        else {
+          res.json(doc);
+        }
+      });
+    });
+
+
+    // Create a new Thought or replace an existing Thought
+    app.post("/articles/:id", function(req, res) {
+      // Create a new Thought and pass the req.body to the entry
+      var newThought = new Thought(req.body);
+
+      // And save the new Thought the db
+      newThought.save(function(error, doc) {
+        // Log any errors
+        if (error) {
+          console.log(error);
+        }
+        // Otherwise
+        else {
+          // Use the article id to find and update it's Thought
+          Article.findOneAndUpdate({ "_id": req.params.id }, { "thought": doc._id })
+          // Execute the above query
+          .exec(function(err, doc) {
+            // Log any errors
+            if (err) {
+              console.log(err);
+            }
+            else {
+              // Or send the document to the browser
+              res.send(doc);
+            }
+          });
+        }
+      });
+    });
+    
+
+    app.get("/delete", function(req, res){
+
+      Article.remove().exec(function(err, data){
+        if (err){
+          console.log(err);
+        } else {
+          res.send("Data Cleared");
+        }
+      });
+
+    });
 };
 
 
 
-/*  // This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-    // Grab every doc in the Articles array
-    Article.find({}, function(error, doc) {
-      // Log any errors
-      if (error) {
-        console.log(error);
-      }
-      // Or send the doc to the browser as a json object
-      else {
-        res.json(doc);
-      }
-    });
-  });
 
-
-  // Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    Article.findOne({ "_id": req.params.id })
-    // ..and populate all of the thoughts associated with it
-    .populate("thought")
-    // now, execute our query
-    .exec(function(error, doc) {
-      // Log any errors
-      if (error) {
-        console.log(error);
-      }
-      // Otherwise, send the doc to the browser as a json object
-      else {
-        res.json(doc);
-      }
-    });
-  });
-  
-  
-  // Create a new Thought or replace an existing Thought
-  app.post("/articles/:id", function(req, res) {
-    // Create a new Thought and pass the req.body to the entry
-    var newThought = new Thought(req.body);
-  
-    // And save the new Thought the db
-    newThought.save(function(error, doc) {
-      // Log any errors
-      if (error) {
-        console.log(error);
-      }
-      // Otherwise
-      else {
-        // Use the article id to find and update it's Thought
-        Article.findOneAndUpdate({ "_id": req.params.id }, { "thought": doc._id })
-        // Execute the above query
-        .exec(function(err, doc) {
-          // Log any errors
-          if (err) {
-            console.log(err);
-          }
-          else {
-            // Or send the document to the browser
-            res.send(doc);
-          }
-        });
-      }
-    });
-  });*/
   
